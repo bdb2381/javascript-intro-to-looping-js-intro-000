b43d61ffbc796eca9cc116f09de0cdca24dee077
Bradley Beggs <brad.beggs@gmail.com>
2020-06-11 05:27:55 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,14 +4,9 @@
 function forLoop(gear){
 
   for (let i = 0; i < 25; i++ ){
-      if (i === 1){
-        //[...gear, "I am 1 strange loop."]
-        gear.push("I am 1 strange loop.")
-      }
+      if (i === 1){ gear.push("I am 1 strange loop.")  }
 
-      else{
-        gear.push('I am ${i} strange loops.')
-      }
+      else {gear.push('I am ${i} strange loops.') }
 
 }
 return array
