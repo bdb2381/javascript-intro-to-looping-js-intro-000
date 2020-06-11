4259701fe92858dee9911c3e7c07f76e0de2db28
Bradley Beggs <brad.beggs@gmail.com>
2020-06-11 05:10:41 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
-let gear = ["cams", "winter tent", "crampons", "helmet"]
+ gear = ["cams", "winter tent", "crampons", "helmet"]
 
 
 function forLoop(gear){
 
   for (let i = 0; i < 25; i++ ){
-      if (i == 1){
+      if (i === 1){
         //[...gear, "I am 1 strange loop."]
         gear.push("I am 1 strange loop.")
       }
