2462207eea6322d85f6fa9b0d6f5fd00d996ec2b
Bradley Beggs <brad.beggs@gmail.com>
2020-06-11 04:56:35 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+let gear = ["cams", "winter tent", "crampons", "helmet"]
+
+
+function forLoop(gear){
+
+  for (let i = 0; i < 25; i++ ){
+      if (i === 1){
+        [...gear, "I am 1 strange loop."]
+      }
+
+      else{
+        [...gear,"I am ${1} strange loops."]
+      }
+      
+}
+return gear
+}
